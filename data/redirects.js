964af0e430411beb093e4ed0974463fab9e013b4
Adam Gincel <agincel@stevens.edu>
2016-07-27 19:34:22 -0400

    Added event/learnjam, updated redirects and event link

--- data/redirects.js
+++ data/redirects.js
@@ -1,9 +1,17 @@
 function getRedirects()
 {
 	return [
+		//game jams
 		{name: "gj6", link: "http://itch.io/jam/sgdc-gj6"},
 		{name: "gj7", link: "http://itch.io/jam/sgdc-gj7"},
 		{name: "gj8", link: "http://itch.io/jam/sgdc-gj8"},
 		{name: "sj3", link: "http://itch.io/jam/sgdc-sj3"},
+
+		//learn jam 16
+		{name: "lj", link: "http://sgdc.ml/event/learnjam"},
+		{name: "lj16", link: "http://sgdc.ml/event/learnjam"},
+		{name: "learnjam", link: "http://sgdc.ml/event/learnjam"},
+
+		//tbd...
 	];
 }
