6afb04d7e749dd5eba291b27fb8daaf175e6322f
Adam Gincel <agincel@stevens.edu>
2016-09-20 22:11:02 -0400

    added gamejam page and gj9 redirect

--- data/redirects.js
+++ data/redirects.js
@@ -2,10 +2,12 @@ function getRedirects()
 {
 	return [
 		//game jams
+		{name: "gj", link: "http://sgdc.ml/events/gamejam"},
 		{name: "gj6", link: "http://itch.io/jam/sgdc-gj6"},
 		{name: "gj7", link: "http://itch.io/jam/sgdc-gj7"},
 		{name: "gj8", link: "http://itch.io/jam/sgdc-gj8"},
 		{name: "sj3", link: "http://itch.io/jam/sgdc-sj3"},
+		{name: "gj9", link: "http://itch.io/jam/sgdc-gj9"},
 
 		//learn jam 16
 		{name: "lj", link: "http://sgdc.ml/events/learnjam"},
