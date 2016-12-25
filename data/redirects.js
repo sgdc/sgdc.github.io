24060bad124d0c47ec8921e627e7c495d9676a5a
Adam Gincel <agincel@stevens.edu>
2016-12-24 22:50:12 -0500

    Update redirects.js

--- data/redirects.js
+++ data/redirects.js
@@ -9,6 +9,7 @@ function getRedirects()
 		{name: "sj3", link: "http://itch.io/jam/sgdc-sj3"},
 		{name: "gj9", link: "http://itch.io/jam/sgdc-gj9"},
 		{name: "gj10", link: "http://itch.io/jam/sgdc-gj10"},
+		{name: "wj", link: "https://itch.io/jam/sgdc-wj1"},
 
 		//learn jam 16
 		{name: "lj", link: "http://sgdc.ml/events/learnjam"},
