f95f0f2332d35843342bf2843faddcbeadaffcb9
Nick <39786661+RayuEternal@users.noreply.github.com>
2018-09-28 16:22:02 -0700

    Added theme for GJC

--- data/events.js
+++ data/events.js
@@ -122,7 +122,7 @@ function getEvents() {
 			{name: "Learn Jam 2018", time: "9/16/18 12-5pm", location: "BC 122", link: "http://sgdc.ml/learnjam"},
 		],
 		present: [
-			{name: "Game Jam C", time: "9/28/18 - 9/30/18", location: "BC 122 & BC104", theme: "TBA", link: "https://itch.io/jam/sgdc-gjc"},
+			{name: "Game Jam C", time: "9/28/18 - 9/30/18", location: "BC 122 & BC104", theme: "Cooperation", link: "https://itch.io/jam/sgdc-gjc"},
 			//really should only be one event here...just in case, I made the 's' conditionally appear if this is longer than 1
 			//don't worry if this is empty, the table will hide itself until it is populated
 			
